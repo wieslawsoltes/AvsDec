4cdb5084b5c57cff27299d55c2fb8f3139fec6b3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-12-25 13:01:39 +0100

    Define streams

--- AvsDec.cpp
+++ AvsDec.cpp
@@ -8,11 +8,14 @@
 
 int main(int argc, char *argv[])
 {
-    _ftprintf(stderr, _T("AvsDec v%s - Decode avisynth audio stream to raw audio file.\n"), VERSION);
+    FILE *log = stderr;
+    FILE *pipe = stdout;
+
+    _ftprintf(log, _T("AvsDec v%s - Decode avisynth audio stream to raw audio file.\n"), VERSION);
 
     if (argc != 3)
     {
-        _ftprintf(stderr, _T("Usage: AvsDec <input.avs> <output.raw>"));
+        _ftprintf(log, _T("Usage: AvsDec <input.avs> <output.raw>"));
         return -1;
     }
 
@@ -24,11 +27,11 @@ int main(int argc, char *argv[])
     CAvs2Raw decoderAVS;
     if (decoderAVS.OpenAvisynth(avsFilePath) == false)
     {
-        _ftprintf(stderr, _T("Failed to initialize Avisynth!"));
+        _ftprintf(log, _T("Failed to initialize Avisynth!"));
         return -1;
     }
 
-    _ftprintf(stderr, _T("Avisynth initialized successfully.\n"));
+    _ftprintf(log, _T("Avisynth initialized successfully.\n"));
 
     AvsAudioInfo infoAVS;
     memset(&infoAVS, 0, sizeof(AvsAudioInfo));
@@ -36,21 +39,21 @@ int main(int argc, char *argv[])
 
     ULONGLONG  nFileSize = infoAVS.nAudioSamples * infoAVS.nBytesPerChannelSample * infoAVS.nAudioChannels;
 
-    _ftprintf(stderr, _T("AVS File Properties\n"));
-    _ftprintf(stderr, _T("Sample format\t: "));
+    _ftprintf(log, _T("AVS File Properties\n"));
+    _ftprintf(log, _T("Sample format\t: "));
     switch (infoAVS.nSampleType)
     {
-    case SAMPLE_INT8: _ftprintf(stderr, _T("SAMPLE_INT8\n")); break;
-    case SAMPLE_INT16: _ftprintf(stderr, _T("SAMPLE_INT16\n")); break;
-    case SAMPLE_INT24: _ftprintf(stderr, _T("SAMPLE_INT24\n")); break;
-    case SAMPLE_INT32: _ftprintf(stderr, _T("SAMPLE_INT32\n")); break;
-    case SAMPLE_FLOAT: _ftprintf(stderr, _T("SAMPLE_FLOAT\n")); break;
-    default: _ftprintf(stderr, _T("unknown\n")); break;
+    case SAMPLE_INT8: _ftprintf(log, _T("SAMPLE_INT8\n")); break;
+    case SAMPLE_INT16: _ftprintf(log, _T("SAMPLE_INT16\n")); break;
+    case SAMPLE_INT24: _ftprintf(log, _T("SAMPLE_INT24\n")); break;
+    case SAMPLE_INT32: _ftprintf(log, _T("SAMPLE_INT32\n")); break;
+    case SAMPLE_FLOAT: _ftprintf(log, _T("SAMPLE_FLOAT\n")); break;
+    default: _ftprintf(log, _T("unknown\n")); break;
     }
-    _ftprintf(stderr, _T("%s\t: %d\n"), _T("Sample rate"), infoAVS.nSamplesPerSecond);
-    _ftprintf(stderr, _T("%s\t: %d\n"), _T("Channels"), infoAVS.nAudioChannels);
-    _ftprintf(stderr, _T("%s\t: %I64d\n"), _T("Audio samples"), infoAVS.nAudioSamples);
-    _ftprintf(stderr, _T("%s\t: %I64d\n"), _T("Decoded size"), nFileSize);
+    _ftprintf(log, _T("%s\t: %d\n"), _T("Sample rate"), infoAVS.nSamplesPerSecond);
+    _ftprintf(log, _T("%s\t: %d\n"), _T("Channels"), infoAVS.nAudioChannels);
+    _ftprintf(log, _T("%s\t: %I64d\n"), _T("Audio samples"), infoAVS.nAudioSamples);
+    _ftprintf(log, _T("%s\t: %I64d\n"), _T("Decoded size"), nFileSize);
 
     int nSamples = 1536;
     Avs2RawStatus statusAVS;
@@ -63,7 +66,7 @@ int main(int argc, char *argv[])
     pBuffer = (FLOAT *)calloc(nSamples * infoAVS.nAudioChannels, sizeof(FLOAT));
     if (pBuffer == nullptr)
     {
-        _ftprintf(stderr, _T("Failed to allocate memory."));
+        _ftprintf(log, _T("Failed to allocate memory."));
         decoderAVS.CloseAvisynth();
         return -1;
     }
@@ -72,19 +75,20 @@ int main(int argc, char *argv[])
 
     if (isOutputPipe == true)
     {
-        fstream = stdout;
-        _setmode(_fileno(stdout), O_BINARY);
+        fstream = pipe;
+        _setmode(_fileno(pipe), O_BINARY);
     }
     else
     {
         errno_t error = fopen_s(&fstream, rawFilePath, "wb");
         if (error != 0)
         {
-            _ftprintf(stderr, _T("Failed to create output file."));
+            _ftprintf(log, _T("Failed to create output file."));
             decoderAVS.CloseAvisynth();
             return -1;
         }
     }
+
 
     int nr;
     ULONGLONG nDecodedSamples = 0;
@@ -96,7 +100,7 @@ int main(int argc, char *argv[])
         fwrite(pBuffer, sizeof(FLOAT), nr * infoAVS.nAudioChannels, fstream);
 
         int nProgress = (int)((100 * nDecodedSamples * infoAVS.nBytesPerChannelSample * infoAVS.nAudioChannels) / nFileSize);
-        _ftprintf(stderr, _T("\rProgress: %d%% (%I64d/%I64d)"), nProgress, statusAVS.nSamples - statusAVS.nSamplesLeft, statusAVS.nSamples);
+        _ftprintf(log, _T("\rProgress: %d%% (%I64d/%I64d)"), nProgress, statusAVS.nSamples - statusAVS.nSamplesLeft, statusAVS.nSamples);
 
     } while (nr > 0);
 
