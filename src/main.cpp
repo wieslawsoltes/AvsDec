27cac9faa15de12d81f887a7af98a43320b393e8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-03 13:47:33 +0100

    Changed option order

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,17 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
-#include "WavInfo.h"
 #include "AvsDec.h"
+#include "WavInfo.h"
 
 void Help(FILE *log)
 {
     _ftprintf(log, _T("usage: AvsDec [option] <...>\n"));
     _ftprintf(log, _T("option:"));
-    _ftprintf(log, _T("\t[-i] Show wav file header info\n"));
-    _ftprintf(log, _T("\tAvsDec -i <input.wav>\n"));
     _ftprintf(log, _T("\t[-d] Decode avisynth audio stream to raw audio file\n"));
     _ftprintf(log, _T("\tAvsDec -d <input.avs> <output.raw>\n"));
+    _ftprintf(log, _T("\t[-i] Show wav file header info\n"));
+    _ftprintf(log, _T("\tAvsDec -i <input.wav>\n"));
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -35,17 +35,17 @@ int _tmain(int argc, _TCHAR* argv[])
         return -1;
     }
 
-    if (_tcsnccmp(option, _T("-i"), 2) == 0 && argc == 3)
-    {
-        _TCHAR *wavFilePath = argv[2];
-        return WavInfo(wavFilePath);
-    }
-
     if (_tcsnccmp(option, _T("-d"), 2) == 0 && argc == 4)
     {
         _TCHAR *avsFilePath = argv[2];
         _TCHAR *rawFilePath = argv[3];
         return AvsDec(avsFilePath, rawFilePath);
+    }
+
+    if (_tcsnccmp(option, _T("-i"), 2) == 0 && argc == 3)
+    {
+        _TCHAR *wavFilePath = argv[2];
+        return WavInfo(wavFilePath);
     }
 
     Help(stderr);
